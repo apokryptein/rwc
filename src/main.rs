b03d02181f1e69bdcfcf68643713fa5b3f722aac
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-04-25 22:37:21 -0400

    feat: column printing, totals

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::Read;
 use std::{fs, io};
 
 #[derive(Parser, Debug)]
@@ -25,64 +26,118 @@ struct Args {
     files: Vec<String>,
 }
 
+struct Result {
+    filename: String,
+    lines: usize,
+    words: usize,
+    bytes: usize,
+}
+
 fn main() {
     // Parse Args
     let args = Args::parse();
 
-    let mut input = String::new();
+    // Vec of Result to store all result from provided files
+    let mut results: Vec<Result> = Vec::new();
+
+    let (mut word_total, mut line_total, mut byte_total) = (0, 0, 0);
 
     if args.files.is_empty() {
+        let mut input = String::new();
         io::stdin()
-            .read_line(&mut input)
+            .read_to_string(&mut input)
             .expect("Failed to read from stdin");
-        parse_input(input.as_str(), "-", args.lines, args.words, args.bytes)
+        results.push(parse_input(input.as_str(), "-"));
     } else {
         for file in &args.files {
             match fs::read_to_string(file) {
-                Ok(content) => parse_input(&content, file, args.lines, args.words, args.bytes),
-                Err(_) => eprintln!("Error reading file: {}", file),
-            }
+                Ok(content) => {
+                    let result = parse_input(&content, file);
+                    line_total += result.lines;
+                    word_total += result.words;
+                    byte_total += result.bytes;
+                    results.push(result);
+                }
+                Err(_) => continue,
+            };
         }
+    }
+
+    // Get largest number to determine column width
+    let largest_value = [line_total, word_total, byte_total]
+        .iter()
+        .copied()
+        .max()
+        .unwrap();
+
+    // Get number of digits in largest number
+    let column_width = largest_value.to_string().chars().count();
+
+    // Print data for each result
+    for res in results {
+        print_file_data(&res, args.lines, args.words, args.bytes, column_width);
+    }
+
+    // Print totals
+    if args.files.len() > 1 {
+        println!(
+            "{0: >column_width$} {1: >column_width$} {2: >column_width$} total",
+            line_total, word_total, byte_total
+        );
     }
 }
 
-fn parse_input(input: &str, file: &str, lines: bool, words: bool, bytes: bool) {
+// Parses input according to selected flags
+fn parse_input(input: &str, file: &str) -> Result {
     // Result Vec
-    let mut result = Vec::new();
+    Result {
+        filename: String::from(file),
+        lines: input.lines().count(),
+        words: input.split_whitespace().count(),
+        bytes: input.len(),
+    }
+}
+
+// Prints data for one processed file
+fn print_file_data(result: &Result, lines: bool, words: bool, bytes: bool, width: usize) {
+    let mut output = Vec::new();
 
     // wc displays results in the following order:
     // Lines Words Bytes Filename
     // If lines flag add data to result
     if lines {
-        result.push(input.lines().count().to_string());
+        output.push(result.lines.to_string());
     }
 
     // If words flag add data to result
     if words {
-        result.push(input.split_whitespace().count().to_string());
+        output.push(result.words.to_string());
     }
 
     // If bytes flag add data to result
     if bytes {
-        result.push(input.len().to_string());
+        output.push(result.bytes.to_string());
     }
 
     // If no flag return all three: L W C Filename
     if !bytes && !lines && !words {
         // Lines
-        result.push(input.lines().count().to_string());
+        output.push(result.lines.to_string());
 
         // Words
-        result.push(input.split_whitespace().count().to_string());
+        output.push(result.words.to_string());
 
         // Bytes
-        result.push(input.len().to_string());
+        output.push(result.bytes.to_string());
     }
 
     // Print result to console
-    if file != "-" {
-        println!("{} {}", result.join(" "), file);
+    if result.filename == "-" {
+        println!("{}", output.join("\t"));
     } else {
-        println!("{}", result.join(" "));
+        println!(
+            "{0: >width$} {1: >width$} {2: >width$}",
+            output[0], output[1], output[2]
+        );
     }
 }
