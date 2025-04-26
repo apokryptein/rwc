5afda3ff82a91603358c6130d90d8f6723799249
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-04-25 21:28:20 -0400

    feat: multi-file support
    Added multi-file support by moving counting logic to its own function
    and checking for the presence of multiple filename arguments in the file
    Vec<String> argument. It loops through all files counting and printing
    parsed values.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
-use std::{fs,io};
+use std::{fs, io};
 
 #[derive(Parser, Debug)]
-#[command(name="rwc", version, about="A small word count utility written in Rust")]
+#[command(
+    name = "rwc",
+    version,
+    about = "A small word count utility written in Rust"
+)]
 struct Args {
     /// Count lines
     #[arg(short = 'l', long)]
@@ -16,68 +20,68 @@ struct Args {
     #[arg(short = 'c', long)]
     bytes: bool,
 
-    /// File 
+    /// File
     #[arg()]
-    file: Option<String>,
+    files: Vec<String>,
 }
 
 fn main() {
     // Parse Args
     let args = Args::parse();
 
-    // Read file into string
-    let contents = match &args.file {
-        Some(filename) => fs::read_to_string(filename).unwrap_or_else(|_| {
-            eprintln!("Error reading file: {}", filename);
-            std::process::exit(1);
-        }),
-        None => {
-            let mut input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Failed to read from stdin");
-            input
-        }
-    };
+    let mut input = String::new();
 
+    if args.files.is_empty() {
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read from stdin");
+        parse_input(input.as_str(), "-", args.lines, args.words, args.bytes)
+    } else {
+        for file in &args.files {
+            match fs::read_to_string(file) {
+                Ok(content) => parse_input(&content, file, args.lines, args.words, args.bytes),
+                Err(_) => eprintln!("Error reading file: {}", file),
+            }
+        }
+    }
+}
+
+fn parse_input(input: &str, file: &str, lines: bool, words: bool, bytes: bool) {
     // Result Vec
     let mut result = Vec::new();
 
-
     // wc displays results in the following order:
     // Lines Words Bytes Filename
-
     // If lines flag add data to result
-    if args.lines {
-        result.push(contents.lines().count().to_string());
+    if lines {
+        result.push(input.lines().count().to_string());
     }
 
     // If words flag add data to result
-    if args.words {
-        result.push(contents.split_whitespace().count().to_string());
+    if words {
+        result.push(input.split_whitespace().count().to_string());
     }
 
     // If bytes flag add data to result
-    if args.bytes {
-        result.push(contents.len().to_string());
+    if bytes {
+        result.push(input.len().to_string());
     }
 
     // If no flag return all three: L W C Filename
-    if !args.bytes && !args.lines && !args.words {
+    if !bytes && !lines && !words {
         // Lines
-        result.push(contents.lines().count().to_string());
+        result.push(input.lines().count().to_string());
 
         // Words
-        result.push(contents.split_whitespace().count().to_string());
+        result.push(input.split_whitespace().count().to_string());
 
         // Bytes
-        result.push(contents.len().to_string());
-
+        result.push(input.len().to_string());
     }
-    
+
     // Print result to console
-    if args.file.is_some() {
-        println!("{} {}", result.join(" "), args.file.as_deref().unwrap_or("-"));
+    if file != "-" {
+        println!("{} {}", result.join(" "), file);
     } else {
         println!("{}", result.join(" "));
     }
